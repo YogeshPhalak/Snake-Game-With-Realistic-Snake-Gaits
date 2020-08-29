50db91ec14c71d87605b8f747f5de0b8e8a551e6
Yogesh Ganesh Phalak <34397502+YogeshPhalak@users.noreply.github.com>
2020-08-29 16:02:07 +0530

    Create SnakeModel.h

---
+++ Snake-Game/SnakeModel.h
@@ -0,0 +1,25 @@
+#ifndef SNAKEMODEL_H_INCLUDED
+#define SNAKEMODEL_H_INCLUDED
+
+#define COLUMNS 10
+#define ROWS 10
+#define MARGIN 2
+
+#define CELLWIDTH 70
+#define GRIDWIDTH 10
+
+#define LIMIT 100.0
+
+#define FPS 20
+
+#define PI 3.1415926535898
+
+void drawEnv(), unit(int, int), drawSnake();
+
+
+
+#define FORWARD 0
+#define RIGHT -0.055
+#define LEFT 0.055
+
+#endif
